ea226e8773ec03a29d527d4f4dc4f319a82d765f
studmaiele5643 <leonhard.maier@stud.th-rosenheim.de>
2024-03-26 14:46:34 +0100

    some tests

---
+++ backend/src/test/java/com/controller/ApiControllerTest.java
@@ -0,0 +1,46 @@
+package com.controller;
+
+import com.sqs_maier.controller.ApiController;
+import com.sqs_maier.service.DataService;
+import com.sqs_maier.util.Autobahn;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApiController.class)
+@AutoConfigureMockMvc
+class ApiControllerTest {
+
+    @MockBean
+    private DataService dataService;
+
+    @Autowired
+    private ApiController apiController;
+
+    @Test
+    void getData_withUnknownRoadId_shouldReturnNotFound() throws IOException {
+        // Arrange
+        String unknownRoadId = "unknown";
+        when(dataService.getData(Autobahn.UNKNOWN)).thenReturn(ResponseEntity.ok("Some data")); // Mock dataService response
+
+        // Act
+        ResponseEntity<String> response = apiController.getData(unknownRoadId);
+
+        // Assert
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+}
