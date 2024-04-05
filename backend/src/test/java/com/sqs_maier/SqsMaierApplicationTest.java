bc561b197210822a8595377d0583978b25f6044c
studmaiele5643 <leonhard.maier@stud.th-rosenheim.de>
2024-04-05 10:36:11 +0200

    add a main Test

---
+++ backend/src/test/java/com/sqs_maier/SqsMaierApplicationTest.java
@@ -0,0 +1,11 @@
+package com.sqs_maier;
+
+import org.junit.jupiter.api.Test;
+
+public class SqsMaierApplicationTest {
+
+        @Test
+        public void mainStartUpTest() {
+            SqsMaierApplication.main(new String[] {});
+        }
+    }
