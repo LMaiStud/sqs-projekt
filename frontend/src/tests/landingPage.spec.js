b202681c55a357945d660e1bf11023f369751eea
studmaiele5643 <leonhard.maier@stud.th-rosenheim.de>
2024-06-11 12:58:03 +0200

    add some test and remove test.use

--- frontend/src/tests/landingPage.spec.js
+++ frontend/src/tests/landingPage.spec.js
@@ -1,23 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-    geolocation: {
-        latitude: 41.890221,
-        longitude: 12.492348
-    },
-    locale: 'it-IT',
-    permissions: ['geolocation'],
-    timezoneId: 'Europe/Rome'
-});
-
-test('TestSucheWitheWrongName', async ({ page }) => {
+test('sucheMitFlaschenSuchbegriff', async ({ page }) => {
     await page.goto('http://localhost/');
     await page.getByLabel('Suche').click();
-    await page.getByLabel('Suche').fill('alskfdjlaskfj');
+    await page.getByLabel('Suche').fill('sadfasfasfd');
     await page.getByRole('button', { name: 'Suche' }).click();
-    await page.getByRole('heading', { name: 'Diese Autobahn "ALSKFDJLASKFJ' }).click();
+    await page.getByRole('heading', { name: 'Diese Autobahn "SADFASFASFD"' }).click();
+});
+
+test('sucheMitEchterA99', async ({ page }) => {
+    await page.goto('http://localhost/');
     await page.getByLabel('Suche').click();
-    await page.getByLabel('Suche').fill('alskfdjlaskfjA');
     await page.getByLabel('Suche').press('ControlOrMeta+a');
     await page.getByLabel('Suche').fill('A99');
     await page.getByRole('button', { name: 'Suche' }).click();
@@ -25,16 +18,60 @@ test('TestSucheWitheWrongName', async ({ page }) => {
     await page.getByRole('columnheader', { name: 'Beschreibung' }).click();
 });
 
-test('test', async ({ page }) => {
+test('leereSuche', async ({ page }) => {
     await page.goto('http://localhost/');
     await page.getByLabel('Suche').click();
     await page.getByRole('button', { name: 'Suche' }).click();
+    await page.getByRole('heading', { name: 'Bitte Baustelle Suchen!' }).click();
+    await page.getByLabel('Suche').click();
+});
+
+test('echteSucheDannFalscheSuche', async ({ page }) => {
+    await page.goto('http://localhost/');
     await page.getByLabel('Suche').click();
     await page.getByLabel('Suche').fill('a99');
     await page.getByRole('button', { name: 'Suche' }).click();
     await page.getByRole('columnheader', { name: 'Standort' }).click();
+    await page.getByRole('columnheader', { name: 'Beschreibung' }).click();
     await page.getByLabel('Suche').click();
-    await page.getByLabel('Suche').fill('a99999');
+    await page.getByLabel('Suche').fill('a9999');
     await page.getByRole('button', { name: 'Suche' }).click();
-    await page.getByRole('heading', { name: 'Diese Autobahn "A99999"' }).click();
+    await page.getByRole('heading', { name: 'Diese Autobahn "A9999"' }).click();
 });
+
+test('zweiMalEchteSucheLetzteFalsch', async ({ page }) => {
+    await page.goto('http://localhost/');
+    await page.getByLabel('Suche').click();
+    await page.getByLabel('Suche').fill('A1');
+    await page.getByRole('button', { name: 'Suche' }).click();
+    await page.getByRole('columnheader', { name: 'Standort' }).click();
+    await page.getByRole('columnheader', { name: 'Beschreibung' }).click();
+    await page.getByLabel('Suche').click();
+    await page.getByLabel('Suche').fill('A99');
+    await page.getByRole('button', { name: 'Suche' }).click();
+    await page.getByRole('columnheader', { name: 'Standort' }).click();
+    await page.getByRole('columnheader', { name: 'Beschreibung' }).click();
+    await page.getByLabel('Suche').click();
+    await page.getByLabel('Suche').fill('alsfjdfsakfj');
+    await page.getByRole('button', { name: 'Suche' }).click();
+    await page.getByRole('heading', { name: 'Diese Autobahn "ALSFJDFSAKFJ' }).click();
+});
+
+test('sucheKleinGrossSchreibung', async ({ page }) => {
+    await page.goto('http://localhost/');
+    await page.getByLabel('Suche').click();
+    await page.getByLabel('Suche').fill('a99');
+    await page.getByRole('button', { name: 'Suche' }).click();
+    await page.getByRole('columnheader', { name: 'Standort' }).click();
+    await page.getByRole('columnheader', { name: 'Beschreibung' }).click();
+    await page.getByLabel('Suche').click();
+    await page.getByLabel('Suche').fill('A99');
+    await page.getByRole('button', { name: 'Suche' }).click();
+    await page.getByRole('columnheader', { name: 'Standort' }).click();
+    await page.getByRole('columnheader', { name: 'Beschreibung' }).click();
+    await page.getByLabel('Suche').click();
+    await page.getByLabel('Suche').fill('A9999');
+    await page.getByRole('button', { name: 'Suche' }).click();
+    await page.getByRole('heading', { name: 'Diese Autobahn "A9999"' }).click();
+});
+
