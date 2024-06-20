d86d05b3f9f0f318d97604673fabc3b9b0145df2
studmaiele5643 <leonhard.maier@stud.th-rosenheim.de>
2024-06-20 21:56:35 +0200

    add frontend tests

---
+++ frontend/src/tests/LandingPage.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, fireEvent, screen, act } from '@testing-library/react';
+import LandingPage from '../LandingPage';
+
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('rendert ohne Fehler', () => {
+        render(<LandingPage />);
+        // Beispielhafte Assertion - anpassen nach Bedarf
+        expect(screen.getByLabelText('Suche')).toBeInTheDocument();
+    });
+
+it('zeigt den Titel korrekt an', () => {
+    render(<LandingPage />);
+    expect(screen.getByText('Beste Autobaustellenauskunft')).toBeInTheDocument();
+});
+
+it('zeigt die Suche korrekt an', () => {
+    render(<LandingPage />);
+    expect(screen.getByText('Bitte Baustelle Suchen!')).toBeInTheDocument();
+});
+
+it('zeigt den Button Suche korrekt an', () => {
+    render(<LandingPage />);
+    const buttonElement = screen.getByRole('button', { name: 'Suche' });
+    expect(buttonElement).toBeInTheDocument();
+});
+
+
