cae4e653270977eead998bbcabdcec9e5a0c9ca3
studmaiele5643 <leonhard.maier@stud.th-rosenheim.de>
2024-06-23 12:08:07 +0200

    remove imports

--- frontend/src/tests/LandingPage.test.tsx
+++ frontend/src/tests/LandingPage.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, screen, act } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import LandingPage from '../LandingPage';
 
 
